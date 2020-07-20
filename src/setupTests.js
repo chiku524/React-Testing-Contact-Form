15a6898b7d4a08f78f0b44161378d6e136e437d9
Nico Chikuji <chiku524@icloud.com>
2020-07-19 20:44:50 -0400

    mvp

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,56 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import 'mutationobserver-shim';
+
+import React from 'react';
+import {render, screen, getByTestId, fireEvent} from '@testing-library/react';
+import ContactForm from './components/ContactForm';
+import App from '../src/App';
+
+test('First name renders correctly', () => {
+    const {getByTestId} = render(<ContactForm />);
+
+    const firstNameInput = screen.getByTestId('first-name');
+
+    fireEvent.change(firstNameInput, {
+        target: {value: 'Nico'}
+    })
+
+    expect(firstNameInput).toHaveValue('Nico')
+})
+
+test('Last name renders correctly', () => {
+    const {getByTestId} = render(<ContactForm />);
+
+    const lastNameInput = getByTestId('lastName');
+
+    fireEvent.change(lastNameInput, {
+        target: {value: 'Chikuji'}
+    })
+
+    expect(lastNameInput).toHaveValue('Chikuji');
+})
+
+test('Email renders correctly', () => {
+    const {getByTestId} = render(<ContactForm />);
+
+    const emailInput = getByTestId('email');
+
+    fireEvent.change(emailInput, {
+        target: {value: 'chiku524@icloud.com'}
+    })
+
+    expect(emailInput).toHaveValue('chiku524@icloud.com');
+})
+
+test('Message renders correctly', () => {
+    const {getByTestId} = render(<ContactForm />);
+
+    const messageInput = getByTestId('message');
+
+    fireEvent.change(messageInput, {
+        target: {value: 'Much testing needed'}
+    })
+
+    expect(messageInput).toHaveValue('Much testing needed');
+})
